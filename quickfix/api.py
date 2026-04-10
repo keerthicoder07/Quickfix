d6eba8f8bb9497257cc42945c6542fcd60e0460f
Keerthi <keerthi.official.007@gmail.com>
2026-04-10 12:57:17 +0530

    feat:compelte section H2

--- quickfix/api.py
+++ quickfix/api.py
@@ -23,6 +23,8 @@ def get_overdue_jobs():
 
 @frappe.whitelist()
 def transfer_job(from_tech: str, to_tech: str) -> None:
+	if from_tech == to_tech:
+		frappe.throw("Can't set already assigned technician")
 	try:
 		frappe.db.sql(
 			"""
--- quickfix/quickfix/doctype/job_card/job_card.js
+++ quickfix/quickfix/doctype/job_card/job_card.js
@@ -6,6 +6,68 @@
 
 // 	},
 // });
+frappe.ui.form.on("Job Card", {
+	setup: function (frm) {
+		frm.set_query("assigned_technician", function () {
+			return {
+				filters: {
+					status: "Active",
+					specialization: frm.doc.device_type,
+				},
+			};
+		});
+	},
+	refresh: function (frm) {
+		frm.add_custom_button("Reject Job", function () {
+			let d = new frappe.ui.Dialog({
+				title: "Reject Job",
+				fields: [
+					{
+						label: "Rejection Reason",
+						fieldname: "reason",
+						fieldtype: "Small Text",
+						reqd: 1,
+					},
+				],
+				primary_action_label: "Submit",
+				primary_action(values) {
+					frappe.msgprint("Rejected: " + values.reason);
+					d.hide();
+				},
+			});
+			d.show();
+		}),
+			frm.add_custom_button("Transfer Technician", function () {
+				frappe.prompt(
+					[
+						{
+							label: "New Technician",
+							fieldname: "technician",
+							fieldtype: "Link",
+							options: "Technician",
+							reqd: 1,
+						},
+					],
+					function (values) {
+						frappe.confirm("Are you sure want to transfer?", function () {
+							frappe.call({
+								method: "quickfix.api.transfer_job",
+								args: {
+									from_tech: frm.doc.assigned_technician,
+									to_tech: values.technician,
+								},
+								callback: function () {
+									frm.set_value("assigned_technician", values.technician);
+
+									frm.trigger("asssigned_technician");
+								},
+							});
+						});
+					}
+				);
+			});
+	},
+});
 
 frappe.realtime.on("job_ready", (data) => {
 	console.log("Job ready:", data);
